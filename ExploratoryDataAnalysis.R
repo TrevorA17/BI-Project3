d7302eddefa83b266256c47c6a2d17cc6f1be447
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-02-12 18:14:54 +0300

    load dataset

---
+++ ExploratoryDataAnalysis.R
@@ -0,0 +1,20 @@
+# Load dataset
+HousingData <- read.csv("data/boston.csv", colClasses = c(
+  CRIM = "numeric",
+  ZN = "numeric",
+  INDUS = "numeric",
+  CHAS = "numeric",
+  NOX = "numeric",
+  RM = "numeric",
+  AGE = "numeric",
+  DIS = "numeric",
+  RAD = "numeric",
+  TAX = "numeric",
+  PTRATIO = "numeric",
+  B = "numeric",
+  LSTAT = "numeric",
+  MEDV = "numeric"
+))
+
+# Display the dataset
+View(HousingData)
